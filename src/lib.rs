d431f238c88db9cfa40c8a849684b2b773873327
Sibyl <metinkorkmaz417@gmail.com>
2022-05-17 01:02:36 +0300

    gamemode to enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,33 @@
 pub mod read;
 
-use std::{fs, io::Result};
-use read::{read_byte, read_string, read_int};
+use read::{read_byte, read_int, read_string};
+use std::{fs, io::Result, mem::transmute};
+
+#[repr(i8)]
+#[derive(Debug)]
+pub enum Gamemode {
+    Standart = 0,
+    Taiko = 1,
+    CatchTheBeat = 2,
+    Mania = 3,
+}
+
+impl Gamemode {
+    fn from_byte(b: u8) -> Gamemode {
+        unsafe { transmute(b) }
+    }
+}
+
+impl Default for Gamemode {
+    fn default() -> Self {
+        Gamemode::Standart
+    }
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Default)]
 pub struct Replay {
-    gamemode: u8,
+    gamemode: Gamemode,
     version: u32,
     beatmap_md5: String,
     username: String,
@@ -27,7 +48,7 @@ pub struct Replay {
     replay_data: Vec<u8>,
     score_id: usize,
     mod_info: Option<f64>,
-    raw: Vec<u8>
+    raw: Vec<u8>,
 }
 
 impl Replay {
@@ -37,15 +58,18 @@ impl Replay {
 
     pub fn read(&mut self, path: &str) -> Result<()> {
         let mut content = fs::read(path)?;
-        
+
         let mut p = 0;
         let p_ref = &mut p;
-    
-        self.gamemode = read_byte(p_ref, &content);
+
+        self.gamemode = Gamemode::from_byte(read_byte(p_ref, &content));
         self.version = read_int!(u32, p_ref, &content);
-        self.beatmap_md5 = read_string(p_ref, &mut content).unwrap_or("Can't read beatmap md5!".to_string());
-        self.username = read_string(p_ref, &mut content).unwrap_or("Can't read username!".to_string());
-        self.replay_md5 = read_string(p_ref, &mut content).unwrap_or("Can't read replay md5!".to_string());
+        self.beatmap_md5 =
+            read_string(p_ref, &mut content).unwrap_or("Can't read beatmap md5!".to_string());
+        self.username =
+            read_string(p_ref, &mut content).unwrap_or("Can't read username!".to_string());
+        self.replay_md5 =
+            read_string(p_ref, &mut content).unwrap_or("Can't read replay md5!".to_string());
         self.n300 = read_int!(u16, p_ref, &content);
         self.n100 = read_int!(u16, p_ref, &content);
         self.n50 = read_int!(u16, p_ref, &content);
@@ -55,7 +79,7 @@ impl Replay {
         self.score = read_int!(u32, p_ref, &content);
         self.combo = read_int!(u16, p_ref, &content);
         self.perfect = read_byte(p_ref, &content);
-        self.mods = read_int!(u32, p_ref,  &content);
+        self.mods = read_int!(u32, p_ref, &content);
         self.life_bar = read_string(p_ref, &mut content).unwrap_or("".to_string());
         self.time_stamp = read_int!(usize, p_ref, &content);
         self.replay_length = read_int!(u32, p_ref, &content);
@@ -64,7 +88,7 @@ impl Replay {
         *p_ref += self.replay_length as usize;
 
         self.score_id = read_int!(usize, p_ref, &content);
-        
+
         if *p_ref != content.len() {
             self.mod_info = Some(read_int!(usize, p_ref, &content) as f64);
         }
