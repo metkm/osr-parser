02beaac742bd35337ac4a8d7ba1e3cc94d9779f4
Sibyl <metinkorkmaz417@gmail.com>
2022-05-20 01:42:52 +0300

    changes to replay test

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,20 +1,19 @@
+
 #[cfg(test)]
 pub mod tests {
-    use std::io::Result;
-
     #[test]
     #[cfg(feature = "lzma")]
-    fn decompress_replay() -> Result<()> {
+    fn decompress_replay() -> std::io::Result<()> {
         use std::io::Read;
 
         let mut replay = osr_parser::Replay::new();
         replay = replay.read("./replay.osr").unwrap();
 
-        let mut content = String::new();
+        let mut content = String::with_capacity(replay.replay_length as usize);
         let mut reader = lzma::Reader::from(&*replay.replay_data).unwrap();
 
-        reader.read_to_string(&mut content)?;
-        println!("{:?}", &content);
+        reader.read_to_string(&mut content);
+        // println!("{:?}", &content);
 
         Ok(())
     }
